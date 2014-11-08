7d4d81b3edd6f60735c08b3f00ca77d1ad19a261
David Kinzer <dtkinzer@gmail.com>
2014-11-08 17:11:15 -0500

    Scratching on hw3.

--- hw3.hs
+++ hw3.hs
@@ -77,3 +77,27 @@ False ||| False = False
 False ||| True = True
 True ||| False = True
 True ||| True = True
+
+-- Exercise 3.
+--True &&& True = True
+--_ &&& _ = False
+
+--a &&& b = if a then if b then True else False else False
+
+--a &&& b = if not (a) then not(b) else True
+
+--a &&& b = if a then b
+
+--a &&& b = if a then if b then False else True else False
+
+--a &&& b = if a then b else False
+
+--a &&& b = if b then a else False
+
+
+mult1 x y z = \ x -> (\ y -> (\ z  -> x * y * z))
+--mult2 = \ x -> (x * \ y -> (y * \ z -> z))
+mult3 = \ x -> ( \y -> ( \z -> x * y * z))
+
+e2 :: Eq t => [[[t]]]
+e2 = [[[1, 2, 3]], [[3, 4, 5]]]
