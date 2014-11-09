10ea93f5d8c88113e059f40eb7b2a6f31339d404
David Kinzer <dtkinzer@gmail.com>
2014-11-08 21:42:19 -0500

    Scratch on hw4e7.

--- hw4.hs
+++ hw4.hs
@@ -20,16 +20,23 @@ positions x xs = find x  (zip xs [0 .. n])
 let2int :: Char -> Int
 let2int c = ord c - ord 'a'
 
+
 int2let :: Int -> Char
 int2let n = chr (ord 'a' + n)
 
 shift :: Int -> Char -> Char
 shift n c
   | isLower c = int2let ((let2int c + n) `mod` 26)
+  | isUpper c = toUpper (shift n (toLower c))
+  | otherwise = c
+
+unshift :: Int -> Char -> Char
+unshift n c
+  | isLower c = int2let ((let2int c - n) `mod` 26)
   | otherwise = c
 
 encode :: Int -> String -> String
 encode n xs = [shift n x | x <- xs]
 
 decode :: Int -> String -> String
-decode n xs = [shift () x | x <- xs]
+decode n xs = [unshift n x | x <- xs]
