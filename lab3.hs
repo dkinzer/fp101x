6caee1dcb2decf96c9a7e3422ab7374133d59656
David Kinzer <dtkinzer@gmail.com>
2014-11-11 17:32:22 -0500

    Add lab3 template.

---
+++ lab3.hs
@@ -0,0 +1,41 @@
+module Lab3 where
+
+-----------------------------------------------------------------------------------------------------------------------------
+-- LIST COMPREHENSIONS
+------------------------------------------------------------------------------------------------------------------------------
+
+-- ===================================
+-- Ex. 0 - 2
+-- ===================================
+
+evens :: [Integer] -> [Integer]
+evens = undefined
+
+-- ===================================
+-- Ex. 3 - 4 
+-- ===================================
+
+-- complete the following line with the correct type signature for this function
+-- squares :: ... 
+squares n = undefined
+
+sumSquares :: Integer -> Integer
+sumSquares n = sum (squares n)
+
+-- ===================================
+-- Ex. 5 - 7
+-- ===================================
+
+-- complete the following line with the correct type signature for this function
+-- squares' :: ...
+squares' m n = undefined
+
+sumSquares' :: Integer -> Integer
+sumSquares' x = sum . uncurry squares' $ (x, x)
+
+-- ===================================
+-- Ex. 8
+-- ===================================
+
+coords :: Integer -> Integer -> [(Integer,Integer)]
+coords = undefined
