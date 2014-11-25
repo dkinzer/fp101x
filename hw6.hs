36535afffb5247df116e8c14d6a0ffff67af1247
David Kinzer <dtkinzer@gmail.com>
2014-11-24 19:23:05 -0500

    e0, e1

--- hw6.hs
+++ hw6.hs
@@ -1,3 +1,25 @@
+module HW6 where
+
+-- e0
+e0 = ([(2 *) x | x <- [1.. 10], even x]) ==  map (2 *) (filter even [1.. 10])
+
 safetail :: [a] -> [a]
 safetail [] = []
 safetail (_:xs) = xs
+
+-- e1
+-- all :: (a -> Bool) -> [a] -> Bool
+all0 :: (a -> Bool) -> [a] -> Bool
+all0 p xs = and (map p xs)
+
+all2 :: (a -> Bool) -> [a] -> Bool
+all2 p = and . map p
+
+all3 :: (a -> Bool) -> [a] -> Bool
+all3 p = not . any (not . p)
+
+all5 :: (a -> Bool) -> [a] -> Bool
+all5 p xs = foldl (&&) True (map p xs)
+
+all7 :: (a -> Bool) -> [a] -> Bool
+all7 p = foldr (&&) True . map p
